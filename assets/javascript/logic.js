ccdf6dfc87235a90b6fe7832aa9d91bcb5425f66
Erika UCI <erikauci@dhcp-v088-072.mobile.uci.edu>
2017-08-10 18:40:47 -0700

    omdb api

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,37 +1,79 @@
-// Variables
 
 // Initialize Firebase
- // var config = {
-    apiKey: "AIzaSyBrUEq3UanjWC7n4WA3k1mcAa_NSwSiiR8",
-    authDomain: "movie-soon.firebaseapp.com",
-    databaseURL: "https://movie-soon.firebaseio.com",
-    projectId: "movie-soon",
+  var config = {
+    apiKey: "AIzaSyBuS8nclrAYanROXUHbjObahizCD09OzRI",
+    authDomain: "moviesoon-99bc7.firebaseapp.com",
+    databaseURL: "https://moviesoon-99bc7.firebaseio.com",
+    projectId: "moviesoon-99bc7",
     storageBucket: "",
-    messagingSenderId: "684685466431"
+    messagingSenderId: "624991343805"
   };
   firebase.initializeApp(config);
-// End of Firebase
 
   var database 	= firebase.database();
+
   var authKeyGiphy = "550aa0ca0e7e4111a77bbb3c150b8351";
-  var authKeyOmdb = "7d3b2da5";
 
   var queryGiphy = "http://api.giphy.com/v1/gifs/trending?api_key=" + authKeyGiphy;
-  var queryOmdb = "http://www.omdbapi.com/?i=tt3896198&apikey=" + authKeyOmdb;
-  
 
-// Functions
-function getGiphy (){
-console.log("Inside Get Giphy function");
 
-$.ajax({
-	url: queryGiphy,
-	method: "GET"
-}).done(function(response){
+  $('#submit').on('click', function(event){
+    console.log("I got clicked");
+    var movie = $(this).attr('data-name');
+    var queryOmbd =  "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=104c64bb";
 
+    event.preventDefault();
+  $.ajax({
+	   url: queryOmbd,
+	    method: "GET"
+    }).done(function(response){
+
+
+        var rating = response.Rated;
+
+        var pRating = $("#movie-data").append("Rating:" + rating);
+
+        $('#movie-data').append(pRating);
+
+        var released = response.Released;
+
+        var pReleased = $("#movie-data").append("Released: " + released);
+
+        $('#movie-data').append(pReleased);
+
+        var plot = response.Plot;
+
+        var pPlot = $("#movie-data").append("Plot " + plot);
+
+        $('#movie-data').append(pPlot)
+
+        var imgURL = response.Poster;
+
+        var image = $("#poster").append("src", imgURL);
+
+        $('#poster').append(image);
+
+      });
 });
-}
-function getOmdb (){
+
+/*$('#submit').on('click', function(event){
+  console.log("I got clicked");
+  event.preventDefault();
+
+  var movie = $('#submitButton').val();
+
+  $('.invisible-container').append(movie);
+});*/
+
+//$(document).on('click', '.movie-data', displayMovieInfo);
+
+  //var movie = $("#userInput").val().trim();
+
+  //$('#movie-data').append(movie);
+
+
+
+/*function getOmdb (){
 console.log("This is GEt OMDB");
 $.ajax({
 	url: queryOmdb,
@@ -50,5 +92,4 @@ $('#submit').on('click', function(){
 	var movieTitle = $('#submit').val();
 getOmdb();
 // getGiphy();
-});
-
+});*/
