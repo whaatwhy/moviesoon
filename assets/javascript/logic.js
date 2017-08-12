40bc12decb225a04b82e336f78ce3885e7b30307
Erika UCI <erikauci@dhcp-v113-100.mobile.uci.edu>
2017-08-12 10:25:33 -0700

    js update

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -14,6 +14,18 @@
 
   //var queryGiphy = "http://api.giphy.com/v1/gifs/trending?api_key=550aa0ca0e7e4111a77bbb3c150b8351";
 
+  function checkReviews() {
+    if(movieReviews = true) { //if(movieReviews > 0)
+      database.ref().orderByChild('TIMESTAMP').limitToLast(10).on("child_added", function(snapshot) {
+        console.log(snapshot.val());
+        var userReview = snapshot.val().movieReview;
+        $(".table > tbody").append('<tr><td>'+ userReview +'</td><tr>');
+      })
+    }
+  }
+
+
+
 
 $('#submit').on('keypress click ', function(event){
 
@@ -52,15 +64,17 @@ $('#submit').on('keypress click ', function(event){
 
       $("#movie-data").empty();
       $("#poster").empty();
-      
+
       $("#movie-data").append("Rating:" + rating);
       $("#movie-data").append("Released: " + released);
       $("#movie-data").append("Plot " + plot);
       $("#poster").append("<img id='theImg' src='" + imgURL + "' />");
 
      });
-   });
 
+
+   checkReviews();
+});
 
 
 $('#submitReviewButton').on('click ', function(event){
@@ -75,12 +89,5 @@ $('#submitReviewButton').on('click ', function(event){
   database.ref().push(reviewOb);
   console.log(review);
   $('#user-review').val('');
-});
-
-database.ref().orderByChild('TIMESTAMP').limitToLast(10).on("child_added", function(snapshot) {
-  console.log(snapshot.val());
-
-  var userReview = snapshot.val().movieReview;
-
-  $(".table > tbody").append('<tr><td>'+ userReview +'</td><tr>');
+  checkReviews(); //Do we want this here?
 });
